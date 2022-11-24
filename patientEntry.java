f848bacac0599ae27487c50beefb5034b834ba2c
Unforturnacy <tericsimons12@gmail.com>
2022-11-24 05:35:39 -0500

    Login Page
    Created Login Frame - Just the skeleton no real added functionality as yet

---
+++ patientEntry.java
@@ -0,0 +1,110 @@
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JEditorPane;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+
+public class patientEntry{
+    private JPanel innerloginPanel;
+    private JPanel innerloginPaneltitle;
+    private JPanel loginPanel;
+    private JTextField user;
+    private JTextField pass;
+    private JButton login;
+    public patientEntry()
+    {
+        final int dimx = 220;
+        JFrame frame = new JFrame();
+        loginPanel = new JPanel();
+
+        loginPanel.setLayout(null);
+        loginPanel.setPreferredSize(new Dimension(1000, 600));
+
+        innerloginPanel = new JPanel();
+        innerloginPanel.setLayout(null);
+        innerloginPanel.setBounds(dimx, 130, 600, 400);
+        innerloginPanel.setBackground(Color.BLUE);
+        
+        innerloginPaneltitle = new JPanel();
+        innerloginPaneltitle.setLayout(null);
+        innerloginPaneltitle.setBounds(dimx, 80, 600, 50);
+        innerloginPaneltitle.setBackground(Color.RED);
+
+        JLabel title  = new JLabel("Administrator Login", SwingConstants.CENTER);
+        title.setBounds(0, 0, 600, 50);
+        innerloginPaneltitle.add(title);
+
+        JPanel shadow = new JPanel();
+        shadow.setLayout(null);
+        shadow.setBounds(0, 0, 600, 5);
+        shadow.setBackground(Color.YELLOW);
+        innerloginPanel.add(shadow);
+        
+        user = new JTextField("   Username");
+        user.setBounds(130, 70, 400, 55);
+        innerloginPanel.add(user);
+
+       JPanel userIcon = new JPanel();
+       userIcon.setBounds(70, 70, 55, 55);
+       innerloginPanel.add(userIcon);
+       JLabel userIcon1 = new JLabel(new ImageIcon("images\\user.png"));
+       userIcon1.setBounds(0, 0, 55, 55);
+       userIcon.add(userIcon1);
+
+
+
+        pass = new JTextField("   Password");
+        pass.setBounds(130, 140, 400, 55);
+        innerloginPanel.add(pass);
+
+        JPanel passIcon = new JPanel();
+        passIcon.setBounds(70, 140, 55, 55);
+        innerloginPanel.add(passIcon);
+        JLabel passIcon1 = new JLabel(new ImageIcon("images\\pass.png"));
+        passIcon1.setBounds(0, 0, 55, 55);
+        passIcon.add(passIcon1);
+
+        JButton login = new JButton("Login");
+        login.setBounds(380, 210, 140, 55);
+        ButtonListener listener = new ButtonListener();
+        login.addActionListener(listener);
+        innerloginPanel.add(login);
+        
+
+        
+        loginPanel.add(innerloginPaneltitle);
+        loginPanel.add((innerloginPanel));
+        frame.add(loginPanel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Login");
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new patientEntry();
+    }
+
+    private class ButtonListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+    
+            //open here
+        }
+
+    }
+}
